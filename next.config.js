1a25b5e7dbb383be7e0222cdb1404bdf11fa869a
juliusmarminge <julius0216@outlook.com>
2024-02-20 20:00:35 +0100

    jiti

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import("./src/env.js");
+import createJiti from "jiti";
+import { fileURLToPath } from "node:url";
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+jiti("./src/env");
 
 /** @type {import("next").NextConfig} */
 const config = {};
